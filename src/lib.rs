dbf3444aeeffa385aa956ea41ca1519d2d8d9011
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2026-02-27 23:49:29 +0100

    testing type strings

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,6 @@ pub struct MyIndicator {
     pub inner: usize,
 }
 
-
 #[allow(unused, unreachable_code)]
 #[cfg(test)]
 mod tests {
@@ -28,13 +27,22 @@ mod tests {
     use super::*;
     #[test]
     fn first_test() {
-
         let mut it = get_lua_types();
 
         let t = it.next().unwrap();
         assert_eq!(t.fields.unwrap()[0].name, "number");
         assert_eq!(t.fields.unwrap()[1].name, "inner");
-        
+    }
 
+    struct Hello;
+
+    fn t<T>() -> &'static str {
+        std::any::type_name::<T>().split("::").last().unwrap()
+    }
+
+    #[test]
+    fn stringify() {
+        let x = t::<Hello>();
+        assert_eq!(x, "Hello")
     }
 }
